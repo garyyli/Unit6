cebf9631571979cc97115ee997c029dfc7447130
garyyli <garyli@hanovernorwichschools.org>
2017-12-06 13:51:37 -0500

    Updated from Brython Server: 12/6/2017 1:51:37 PM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,15 @@
+#Gary Li
+#12/6/17
+#longestDictionaryWord.py
+
+dictionary = open('engmix.txt')
+
+longestWord = 0
+i = ''
+for word in dictionary:
+    if len(word)>=longestWord:
+        longestWord = len(word)
+        i = word
+
+print(i)
 
