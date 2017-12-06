f31450073c414d31fa6f05cd2ec16c460d3e75b7
garyyli <garyli@hanovernorwichschools.org>
2017-12-06 13:59:17 -0500

    Updated from Brython Server: 12/6/2017 1:59:17 PM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,14 @@
+#Gary Li
+#12/6/17
+#howManyWords.py
 
+dictionary = open('engmix.txt')
+if len(words)=1:
+    
+wordCount = 0
+for word in dictionary:
+    if 'gary' in word:
+        print(word.strip())
+    wordCount += 1
+    
+print('There are', wordCount, 'words in the dicionary.')
