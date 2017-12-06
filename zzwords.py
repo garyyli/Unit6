a85d37e43656a081b23433750a73de3351488f28
garyyli <garyli@hanovernorwichschools.org>
2017-12-06 13:54:32 -0500

    Updated from Brython Server: 12/6/2017 1:54:32 PM

--- zzwords.py
+++ zzwords.py
@@ -1,1 +1,12 @@
+#Gary Li
+#12/6/17
+#zzwords.py
+
+dictionary = open('engmix.txt')
+
+wordCount = 0
+for word in dictionary:
+    if 'zz' in word:
+        print(word.strip())
+    wordCount += 1
 
