e817c3fa2bb7442591a5b1d4e9ced84cbdd874c6
garyyli <garyli@hanovernorwichschools.org>
2017-12-08 14:42:16 -0500

    Updated from Brython Server: 12/8/2017 2:42:17 PM

--- hw.py
+++ hw.py
@@ -1,4 +1,4 @@
-dictionary = open('engmix.txt')
+"""dictionary = open('engmix.txt')
 tf = False
 word = input('Enter a word: ')
 for words in dictionary:
@@ -8,4 +8,12 @@ for words in dictionary:
 if tf == True:
     print(word, 'is in the dictionary')
 else:
-    print(word, 'is not in the dictionary')
+    print(word, 'is not in the dictionary')"""
+
+dictionary = open('engmix.txt')
+L = []
+for words in dictionary:
+    L.append(words.split())
+    
+num = int(input('Enter a number: '))
+print(L[num-1])
