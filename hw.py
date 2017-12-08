9c3fbee26af4235481a7218d33635666a3265351
garyyli <garyli@hanovernorwichschools.org>
2017-12-08 14:54:19 -0500

    Updated from Brython Server: 12/8/2017 2:54:19 PM

--- hw.py
+++ hw.py
@@ -10,10 +10,21 @@ if tf == True:
 else:
     print(word, 'is not in the dictionary')"""
 
-dictionary = open('engmix.txt')
+"""dictionary = open('engmix.txt')
 L = []
 for words in dictionary:
     L.append(words.split())
     
 num = int(input('Enter a number: '))
-print(L[num-1])
+print(L[num-1])"""
+
+"""fileName = input('Enter a file name: ')
+file = open(fileName)
+fileLines = []
+for line in file:
+    fileLines.append(line.strip())
+fileLines.reverse()
+for item in fileLines:
+    print(item+'!')"""
+
+ch = input('Enter a letter: ')
