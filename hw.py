baa6720a96663bed6e4e6b30b649ddb51d982b66
garyyli <garyli@hanovernorwichschools.org>
2017-12-08 14:36:36 -0500

    Updated from Brython Server: 12/8/2017 2:36:36 PM

--- hw.py
+++ hw.py
@@ -1,1 +1,11 @@
-print("Hello, world.")
+dictionary = open('engmix.txt')
+tf = False
+word = input('Enter a word: ')
+for words in dictionary:
+    if wods == words:
+        tf = True
+        
+if tf == True:
+    print(word, 'is in the dictionary')
+else:
+    print(word, 'is not in the dictionary')
