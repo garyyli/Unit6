13cb96fef943090097373a3867542d2691fd1b44
garyyli <garyli@hanovernorwichschools.org>
2017-12-11 14:22:28 -0500

    Updated from Brython Server: 12/11/2017 2:22:28 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,11 @@
+#Gary Li
+#12/11/17
+#warmup16.py
 
+dictionary = open('engmix.txt')
+
+for words in dictionary:
+    if words.strip() != '':
+        if words.strip()[0]= 'g':
+            if words.strip()[-1]= 'l':
+                print(words)
