ef692d4fb8d5871b8063c4b530e0fe40736afeab
garyyli <garyli@hanovernorwichschools.org>
2017-12-13 13:26:25 -0500

    Updated from Brython Server: 12/13/2017 1:26:20 PM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,14 @@
+#Gary Li
+#12/13/17
+#quiz6.py
 
+dictionary = open('engmix.txt')
+
+#program2
+for words in dictionary:
+    i = 0
+    if words.strip() != '':
+        if words.strip()[0]== 'g' and words.strip()[-1]== 'l':
+            i=i+1
+    return i
+print(i)
